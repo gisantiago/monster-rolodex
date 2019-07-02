1c5982fcb9b181a748a2b8753371a115ec32b392
gisantiago <gisantiago@live.com>
2019-07-01 21:56:10 -0400

    Create Card-List and Setup Card-List CSS

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      mosnters: [
-        {
-          name: 'Frankenstein',
-          id: 1
-        },
-        {
-          name: 'Dracula',
-          id: 2
-        },
-        {
-          name: 'Zombie',
-          id: 3
-        }
-      ]
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState({ monsters: users }));
   }
   render() {
     return (
       <div className='App'>
-       {
-         this.state.mosnters.map(monster => (
-           <h1 key={monster.id}>{monster.name}</h1>
-         ))
-       }
+        <CardList>
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
