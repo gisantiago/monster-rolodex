8f8f43a7fb57e615c7fce193ff8b34a9bfd65bd5
gisantiago <gisantiago@live.com>
2019-07-03 12:43:26 -0400

    Updates

---
+++ precache-manifest.ca524db1a2cf8b7d5b7d1fb14b1a404e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac44cb46084a0bcd1f778eaeb54898a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "01fdddd1fd07699a012a",
+    "url": "/monsters-rolodex/static/css/main.505caefa.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "01fdddd1fd07699a012a",
+    "url": "/monsters-rolodex/static/js/main.956c4e95.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
